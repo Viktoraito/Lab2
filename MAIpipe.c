85a9b2d2459290cd934729eeac53c391cc86d3bb
Viktoraito <kkof@rambler.ru>
2017-02-25 03:17:20 -0800

    Added initialisation procedure
    Signed-off-by: Viktoraito <kkof@rambler.ru>

--- MAIpipe.c
+++ MAIpipe.c
@@ -3,6 +3,7 @@
 #include <linux/moduleparam.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Lab 2 pipe module");
@@ -20,6 +21,8 @@ static ssize_t pipe_read(struct file *, char *, size_t, loff_t *);
 static ssize_t pipe_write(struct file *, const char *, size_t, loff_t *);
 
 static int major;
+static char *buf;
+int buf_cur=0;
 
 static struct file_operations fops =
 {
@@ -30,14 +33,20 @@ static struct file_operations fops =
 };
 
 static int __init pipe_init(void) {
+	buf = kmalloc(buf_size, GFP_KERNEL);
+	if(!buf) {
+		pr_alert("Initialising buffer failed with %d size",buf_size);
+		return -1;
+	}
 	pr_warning("Initialising buffer with %d size\n",buf_size);
-	/*initiate buffer*/
+	
 	major = register_chrdev(0, DEVICE_NAME, &fops);
 	if(major < 0) {
 		pr_alert("Registering the character device failed with %d\n", major);
 		return -1;
 	}
 	pr_warning("MAIpipe module is loaded\n");
+
 	pr_warning("You should create dev file with 'mknod /dev/MAIpipe c %d0'\n", major);
 	return 0;
 }
